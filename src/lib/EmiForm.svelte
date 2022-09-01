3f79edbabf05cdac1ac34ce995e2e970db40b581
Abhinn Pandey <abiinkun@gmail.com>
2022-09-01 21:09:26 +0530

    Initial App Work

---
+++ src/lib/EmiForm.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "carbon-components-svelte/css/all.css";
+    import {Button, Slider, Form} from 'carbon-components-svelte'
+  </script>
+  
+  <main>
+    
+    <Form>
+      <Slider min={0} max={100000000000} maxLabel='not infinity' minLabel='0' />
+      <Button>Calculate</Button>
+    </Form>
+    
+  </main>
+  
+  <style>
+  
+  </style>
+  
